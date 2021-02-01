2434a73ec7ff51d10a9f26b2197cb23c4519b0a4
Enesayyildiz1 <66443194+Enesayyildiz1@users.noreply.github.com>
2021-02-01 18:05:58 +0300

    Add project files.

---
+++ NLayerProject/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NLayerProject
+{
+    public class Class1
+    {
+    }
+}
---
+++ NorthWind.Entities/Concrete/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.Concrete
+{
+    public class Product
+    {
+
+    }
+}
